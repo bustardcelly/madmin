a9a4fa7e579eae4e722fb0b2256a8833f1ae8525
Timon Reinhard <timon@timons-imac.speedport_w_723v_1_32_000>
2014-03-27 12:55:50 +0100

    Make CORS work when using credentials.

--- index.js
+++ index.js
@@ -26,9 +26,10 @@ app.use( express.bodyParser() );
 // ## CORS middleware
 // see: http://stackoverflow.com/questions/7067966/how-to-allow-cors-in-express-nodejs
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Origin', req.header('Origin'));
+    res.header('Access-Control-Allow-Methods', 'OPTIONS,GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
       
     // intercept OPTIONS method
     if ('OPTIONS' == req.method) {
